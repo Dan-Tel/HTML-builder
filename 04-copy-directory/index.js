ffa41cb20642c7767669bc11207ae5f2b8118555
Daniyar Telengutov <daniyart2005@gmail.com>
2021-11-03 19:01:08 +0300

    feat: do task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,14 @@
+const path = require('path');
+const fs = require('fs');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, files) => {
+  files.forEach(file => {
+    fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+      if (err) throw err;
+    })
+  })
+})
